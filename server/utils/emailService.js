679020108550c3e37e1fa29cee6f4189f7df35d3
krishna-deo <krishnadeo11096@gmail.com>
2026-09-20 12:15:31 +0530

    Force ipv4first DNS result order to prevent ENETUNREACH IPv6 errors in Nodemailer

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -1,3 +1,7 @@
+const dns = require('dns');
+if (dns.setDefaultResultOrder) {
+    dns.setDefaultResultOrder('ipv4first');
+}
 const nodemailer = require('nodemailer');
 
 let cachedTransporter = null;
